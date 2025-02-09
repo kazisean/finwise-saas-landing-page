4aeaf4525161e2dc9f1157dcc26bb18a7c6ce4c9
kazisean <keh8423@nyu.edu>
2025-02-09 01:14:32 -0500

    SavE

--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -1,5 +1,5 @@
 export const siteDetails = {
-    siteName: 'Finwise',
+    siteName: 'LoanMe',
     siteUrl: 'https://finwise-omega.vercel.app/',
     metadata: {
         title: 'Finwise - Next.js and Tailwind CSS Landing Page Template',
